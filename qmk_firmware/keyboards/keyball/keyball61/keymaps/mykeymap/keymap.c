0623f0c2c0b605977fab82b5d7fe35f4f43f969f
knttnk <kenta.aquarius.tbn@gmail.com>
2022-09-30 01:33:41 +0900

    keymap変更 レイヤー操作，F13以降，マウスボタン45

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mykeymap/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mykeymap/keymap.c
@@ -24,11 +24,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0          , JP_MINS  ,
-    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P          , KC_BSPC  ,
-    KC_LSFT  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KK_SCRL_SCLN  , KC_ENTER ,
-    KC_LCTL  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_BTN3  ,            KC_SPACE , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH       , KC_RSFT  ,
-    MO(2)    , MO(3)    , KC_LWIN  , KC_LALT  , JP_MHEN  , KC_BTN1  , KC_BTN2  ,            MO(1)    , JP_HENK  , _______  , _______  , _______  , SCRL_MO       , KC_RCTRL
+    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4          , KC_5     ,                                  KC_6          , KC_7     , KC_8     , KC_9     , KC_0          , JP_MINS  ,
+    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R          , KC_T     ,                                  KC_Y          , KC_U     , KC_I     , KC_O     , KC_P          , KC_BSPC  ,
+    KC_LSFT  , KC_A     , KC_S     , KC_D     , KC_F          , KC_G     ,                                  KC_H          , KC_J     , KC_K     , KC_L     , KK_SCRL_SCLN  , KC_ENTER ,
+    KC_LCTL  , KC_Z     , KC_X     , KC_C     , KC_V          , KC_B     , KC_BTN3  ,            KC_SPACE , KC_N          , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH       , KC_RSFT  ,
+    MO(2)    , MO(3)    , KC_LWIN  , KC_LALT  , LT(1,JP_MHEN) , KC_BTN1  , KC_BTN2  ,            MO(2)    , LT(3,JP_HENK) , _______  , _______  , _______  , KC_RCTRL      , KC_RCTRL
   ),
 
   [1] = LAYOUT_universal(
@@ -36,14 +36,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , _______  , _______  , KC_UP    , _______  , _______  ,                                  _______  , _______  , _______  , JP_AT    , JP_LBRC  , KC_DEL   ,
     _______  , _______  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,                                  _______  , _______  , JP_SCLN  , JP_COLN  , JP_RBRC  , _______  ,
     _______  , _______  , _______  , KC_DOWN  , _______  , _______  , _______  ,            _______  , _______  , JP_COMM  , JP_DOT   , JP_SLSH  , JP_BSLS  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
+    _______  , _______  , _______  , _______  , _______  , KC_BTN4  , KC_BTN5  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
   ),
 
   [2] = LAYOUT_universal(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , _______  ,
-    _______  , KC_F11   , KC_F12   , KC_F13   , KC_F14   , KC_F15   ,                                  KC_F16   , KC_F17   , KC_F18   , KC_F19   , KC_F20   , _______  ,
-    _______  , _______  , KC_BRIU  , KC_VOLU  , KC_HOME  , KC_PGUP  ,                                  _______  , KC_WBAK  , KC_WFWD  , _______  , _______  , _______  ,
-    _______  , _______  , KC_BRID  , KC_VOLD  , KC_END   , KC_PGDN  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , KC_F11   , KC_F12   , KC_VOLU  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , KC_BRIU  , KC_VOLD  , KC_HOME  , KC_PGUP  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , KC_BRID  , KC_MUTE  , KC_END   , KC_PGDN  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
   ),
 
