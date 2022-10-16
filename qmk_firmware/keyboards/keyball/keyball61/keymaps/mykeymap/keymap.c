2d8709b72bb27acd1377c30bbdc49d0aba60199a
knttnk <kenta.aquarius.tbn@gmail.com>
2022-10-17 07:35:40 +0900

    Jをスクロールに

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mykeymap/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mykeymap/keymap.c
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4          , KC_5     ,                            KC_6          , KC_7     , KC_8     , KC_9     , KC_0          , JP_MINS  ,
-    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R          , KC_T     ,                            KC_Y          , KC_U     , KC_I     , KC_O     , KC_P          , KC_BSPC  ,
+    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4          , KC_5     ,                            KC_6          , KC_7     , KC_8     , KC_9     , KC_0          , KC_BSPC  ,
+    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R          , KC_T     ,                            KC_Y          , KC_U     , KC_I     , KC_O     , KC_P          , JP_MINS  ,
     KC_LSFT  , KC_A     , KC_S     , KC_D     , KC_F          , KC_G     ,                            KC_H          , KC_J     , KC_K     , KC_L     , KK_SCRL_SCLN  , KC_ENTER ,
     KC_LCTL  , KC_Z     , KC_X     , KC_C     , KC_V          , KC_B     , KC_BTN3  ,      KC_SPACE , KC_N          , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH       , KC_RSFT  ,
     XXXXXXX  , XXXXXXX  , KC_LWIN  , KC_LALT  , LT(1,JP_MHEN) , KC_BTN1  , KC_BTN2  ,      MO(2)    , LT(3,JP_HENK) , XXXXXXX  , XXXXXXX  , XXXXXXX  , KC_RCTRL      , KC_RCTRL
