5391ab83455021fd214ed141d2302173334ff998
knttnk <61683744+knttnk@users.noreply.github.com>
2025-11-15 01:55:46 +0900

    config.hを，デフォルトのを参考に追加

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mykeymap/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mykeymap/config.h
@@ -1,7 +1,6 @@
 /*
 This is the c configuration file for the keymap
 
-Copyright 2022 @Yowkees
 Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
 
 This program is free software: you can redistribute it and/or modify
@@ -25,3 +24,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define SPLIT_TRANSPORT_MIRROR
 #define SPLIT_LAYER_STATE_ENABLE  // slave側にも情報送信
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_TWINKLE
+#endif
